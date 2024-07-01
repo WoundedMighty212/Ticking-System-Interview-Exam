85d5b63e0214b7c56a4489e588a77ed9f9d903ea
Michael Ludick <michael.ludick13@outlook.com>
2024-07-01 11:50:34 +0200

    Models Added

---
+++ Models/Bug.cs
@@ -0,0 +1,11 @@
+﻿namespace Ticking_System_Interview_Exam.Models
+{
+    public class Bug
+    {
+        public int BugId { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; } = "Open"; // Open or Resolved
+        public int CreatedByUserId { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Ticking_System_Interview_Exam.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Role { get; set; } // QA or RD
+    }
+}
